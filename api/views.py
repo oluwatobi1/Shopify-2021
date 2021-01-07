1540a12cd0225611888b0e5ba4383d486a9dd6d0
oluwatobi1 <akintunlesetobi@gmail.com>
2021-01-07 17:10:09 +0100

    Created Forms.py for frontend

--- api/views.py
+++ api/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render, HttpResponse
-from .models import Shop
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 from django.views import View
+from .forms import ShopForm
+from .models import Shop
 
 # Create your views here.
 
 
 class ShopView(View):
+    form_class = ShopForm()
+    template_name = "api/index.html"
 
-    def get(self, request):
-        return render(request, "api/index.html")
+    def get(self, request, *args, **kwargs):
+        form = self.form_class
+        context = {'form':form}
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/success/')
+        return render(request, self.template_name, {'form': form})
+
 
