16d3f7a2e0a25ed3fc32bd7a1eb5b47d960d4401
oluwatobi1 <akintunlesetobi@gmail.com>
2021-01-10 19:55:05 +0100

    General Nav links

--- api/urls.py
+++ api/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path('', ShopFormView.as_view(), name='upload_url'),
     path('gallery', ShopGalleryView.as_view(), name='gallery_url'),
     path('signup', SignUpFormView.as_view(), name='signup_url'),
-    path('login', LoginView.as_view(), name='login'),
+    path('login', LoginView.as_view(), name='login_url'),
 
 ]
