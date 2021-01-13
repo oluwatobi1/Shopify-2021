40b9aef7929cd0a5cd83ce088fabe7c238762398
oluwatobi1 <akintunlesetobi@gmail.com>
2021-01-13 17:06:37 +0100

    added personal_gallery template

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,8 @@ from .views import ShopFormView, ShopGalleryView, SignUpFormView, LoginView, log
 
 urlpatterns = [
     path('', ShopFormView.as_view(), name='upload_url'),
-    path('gallery', ShopGalleryView.as_view(), name='gallery_url'),
+    path('gallery', ShopGalleryView.as_view(), name='central_gallery_url'),
+    path('my_gallery', ShopGalleryView.as_view(), name='personal_gallery_url'),
     path('signup', SignUpFormView.as_view(), name='signup_url'),
     path('login', LoginView.as_view(), name='login_url'),
     path('logout', logout_view, name="logout_url"),
