07f55408ab92c6cc02e506b49b47d92bd0b3379f
oluwatobi1 <akintunlesetobi@gmail.com>
2021-01-13 16:18:20 +0100

    responsive navba

--- shopify/settings.py
+++ shopify/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'api/static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'shop')
 MEDIA_URL = '/shop/'
